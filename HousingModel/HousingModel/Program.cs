502d9ba7ddfd2f70cc49d5c5cb239054b8122106
Frantz Paul <fpaul787@gmail.com>
2020-10-23 23:23:15 -0400

    New Project

---
+++ HousingModel/HousingModel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HousingModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
