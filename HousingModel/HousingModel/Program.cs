77c32a81664115e055586044ed5ee982fd2e69c9
Frantz Paul <fpaul787@gmail.com>
2020-10-23 23:26:12 -0400

    Created ML model. ML.NET is cool and easy

--- HousingModel/HousingModel/Program.cs
+++ HousingModel/HousingModel/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HousingModelML.Model;
+using System;
 
 namespace HousingModel
 {
@@ -6,7 +7,23 @@ namespace HousingModel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Input data
+            var input = new ModelInput();
+            input.Longitude = -122.25F;
+            input.Latitude = 37.83F;
+            input.Housing_median_age = 52;
+            input.Total_rooms = 1688;
+            input.Total_bedrooms = 300;
+            input.Population = 150;
+            input.Households = 150;
+            input.Median_income = 2.125F;
+            input.Median_house_value = 191300;
+            input.Ocean_proximity = "NEAR BAY";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Prediction: {result.Score}");
         }
     }
 }
+
